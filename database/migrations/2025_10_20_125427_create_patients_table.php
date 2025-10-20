004cb17a34e0072c56096c39a0eaa1ca3faf2939
bulyan <boelchan@live.com>
2025-10-20 20:51:31 +0700

    migration

---
+++ database/migrations/2025_10_20_125427_create_patients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('nik', 16)->unique();
+            $table->string('no_rm')->nullable();
+            $table->string('ihs_number')->nullable();
+            $table->string('full_name');
+            $table->string('dob');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
