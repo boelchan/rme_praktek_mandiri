45bb04c7443db58c96ca4989e42762193c214411
bulyan <boelchan@live.com>
2025-10-20 21:24:11 +0700

    hbnjm

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,25 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Encounter;
 use App\Models\Rekap;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
-    public $total_tahun;
-
     public $total_bulan;
-
-    public $total_bulan_lalu;
 
     public $total_hari;
 
     public function mount()
     {
-        $this->total_tahun = Rekap::total_tahun();
-        $this->total_bulan = Rekap::total_bulan();
-        $this->total_bulan_lalu = Rekap::total_bulan_lalu();
-        $this->total_hari = Rekap::total_hari();
+        $this->total_bulan = Encounter::total_bulan();
+        $this->total_hari = Encounter::total_hari();
     }
 
     public function render()
