778da8ce60239ac397c697584a45f394e32e40c4
bulyan <38100527+boelchan@users.noreply.github.com>
2025-10-21 14:13:53 +0700

    asdgjkaskdjh

--- app/Livewire/Encounter/EncounterCreate.php
+++ app/Livewire/Encounter/EncounterCreate.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class EncounterCreate extends Component
 {
+    public $encounter_date;
+
+    public function mount()
+    {
+        $this->encounter_date = date('Y-m-d');
+    }
+
     public function render()
     {
         return view('livewire.encounter.encounter-create');
--- resources/views/livewire/encounter/encounter-create.blade.php
+++ resources/views/livewire/encounter/encounter-create.blade.php
@@ -1,3 +1,25 @@
-<div>
-    {{-- The whole world belongs to you. --}}
-</div>
+<x-layouts.content title="Kunjungan">
+    <div class="card w-84 border border-neutral-300">
+        <div class="card-body">
+            <h2 class="card-title">Tambah Data</h2>
+            <form wire:submit="store">
+                <x-form.input label="Tanggal" model="encounter_date" type="date" required />
+                <x-form.input label="Keluhan" model="condition_keluhan" />
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">Tekanan Darah </legend>
+                    <label class="input">
+                        <input type="text" wire:model="observation_sistolik" placeholder="sistolik" />
+                        <span class="label">/</span>
+                        <input type="text" wire:model="observation_diastolik" placeholder="diastolik" />
+                    </label>
+                </fieldset>
+                <x-form.textarea label="Lab" model="specimen" />
+                <x-form.textarea label="Obat" model="medication" />
+                <x-form.textarea label="Keterangan" model="encounter_keterangan" />
+
+                <button class="btn btn-primary btn-soft mt-4">Simpan</button>
+            </form>
+
+        </div>
+    </div>
+</x-layouts.content>
